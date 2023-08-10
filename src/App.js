1a11ccf2e978acf95e18172c109cb84a60c4c86b
Harshal Shah <95232641+harshal8869@users.noreply.github.com>
2023-08-11 00:54:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
- import React from "react";
+import React from "react";
 import Header from "./components/Header";
 import { Route, Routes } from 'react-router-dom';
 import Home from "./components/Home";
